c85b539981494441097d2b7b2daceb2a104c1ad1
Matt Currier <mcurrier@pcs.local>
2019-03-23 22:06:30 -0500

    browser bit

--- test/test_graph.js
+++ test/test_graph.js
@@ -31,3 +31,22 @@ console.log(graf.prettyTopology)
 console.log(graf)
 
 console.log(graf.activate([0.1,0.5,1.0]))
+
+/*
+	0	3	6
+	1	4	7
+	2	5	8
+*/
+graf = new Ptron.graph([
+	[3,4,5],
+	[3,4,5],
+	[3,4,5],
+	[6,7,8],
+	[6,7,8],
+	[6,7,8],
+	[],
+	[],
+	[]
+])
+
+graf.activate([0.1,0.2,0.3])
